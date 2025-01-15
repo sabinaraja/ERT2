4dc282cf7d34540c55ef1effe9ab90b53b642926
sabinaraja <122044810+sabinaraja@users.noreply.github.com>
2025-01-15 14:41:43 -0600

    Add files via upload

--- ERT2_lastrun.py
+++ ERT2_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.3),
-    on Wed Jan 15 14:05:47 2025
+    on Wed Jan 15 14:36:14 2025
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
